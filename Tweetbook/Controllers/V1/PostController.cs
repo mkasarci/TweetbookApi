b1b5bcb559d69ef0d8ed8d142d0f5d57c18997fe
Muhammet Kasarci <muhammedkasarci@gmail.com>
2020-07-13 15:36:25 +0300

    Add project files.

---
+++ Tweetbook/Controllers/V1/PostController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Tweetbook.Contracts.V1;
+using Tweetbook.Domain;
+
+namespace Tweetbook.Controllers.V1
+{
+    public class PostController : Controller
+    {
+        private List<Post> _posts;
+
+        public PostController()
+        {
+            _posts = new List<Post>();
+            for (int i = 0; i < 5; i++)
+            {
+                _posts.Add(new Post{Id = Guid.NewGuid().ToString()});
+            }
+        }
+
+        [HttpGet(ApiRoutes.Posts.GetAll)]
+        public IActionResult GetAll()
+        {
+            return Ok(_posts);
+        }
+    }
+}
