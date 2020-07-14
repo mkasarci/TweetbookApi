a1fb03e0e30f6bd348ca79e3c2253c83d0bfc29a
Muhammet Kasarci <muhammedkasarci@gmail.com>
2020-07-14 21:58:21 +0300

    Create EF Core migrations

--- Tweetbook/Contracts/V1/Requests/CreatePostRequest.cs
+++ Tweetbook/Contracts/V1/Requests/CreatePostRequest.cs
@@ -4,6 +4,6 @@ namespace Tweetbook.Contracts.V1.Requests
 {
     public class CreatePostRequest
     {
-        public string Id { get; set; }
+        public string Name { get; set; }
     }
 }
--- Tweetbook/Startup.cs
+++ Tweetbook/Startup.cs
@@ -27,13 +27,11 @@ namespace Tweetbook
 
         public IConfiguration Configuration { get; }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
             services.InstallServicesInAssembly(Configuration);
         }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
             if (env.IsDevelopment())
